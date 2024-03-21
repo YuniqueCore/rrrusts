28f84a9081f9077cd3e463e2db3a280dc289b061
--global <--global>
2024-03-21 23:39:13 +0800

    complete the Todo Record

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,32 @@
 use clap::Parser;
-use rrrust::todo::{cli::{Cli, Verbs}, dbguard::{DBConfig, DBOperator}, tasks::TodoRecord};
+use rrrust::todo::{
+    cli::{Cli, Verbs},
+    dbguard::{DBConfig, DBOperator},
+    tasks::TodoRecord,
+};
 
 fn main() {
     let db_header = TodoRecord::get_csv_header();
     let db_user = String::from("root");
-    let dbconf = DBConfig::new("./",db_user , db_header);
+    let dbconf = DBConfig::new("./", db_user, db_header);
 
     let args = Cli::parse();
 
     match args.command {
         Verbs::About => {
             println!("A yunique rust todo application was built to construct world.");
-        },
-        Verbs::Add { 
-            title, 
-            description, 
-            due_date, 
-            priority, 
-            done } => {
-                let mut db_op = DBOperator::connect(&dbconf).unwrap();
-                db_op.add_record(TodoRecord::new(0, title, description, priority));
-            },
-        Verbs::Done { 
-            id ,
-            title, 
-        } => todo!(),
-        Verbs::List => {
-
-        },
+        }
+        Verbs::Add {
+            title,
+            description,
+            due_date,
+            priority,
+            done,
+        } => {
+            let mut db_op = DBOperator::connect(&dbconf).unwrap();
+            db_op.add_record(TodoRecord::new(0, title, description, priority));
+        }
+        Verbs::Done { id, title } => todo!(),
+        Verbs::List => {}
     }
 }
--- src/todo/tasks.rs
+++ src/todo/tasks.rs
@@ -1,5 +1,9 @@
-use std::{collections::{hash_map, HashMap}, fs};
+use std::{
+    fs,
+    io::{BufRead, BufReader},
+};
 
+use anyhow::{bail, Context};
 use chrono::{DateTime, Local};
 
 use super::dbguard::DBConfig;
@@ -29,7 +33,9 @@ const HEADER_MAP: &[(&str, u8)] = &[
 ];
 
 pub fn get_header_index(header: &str) -> Option<u8> {
-    HEADER_MAP.iter().find_map(|(h, i)| if *h == header { Some(*i) } else { None })
+    HEADER_MAP
+        .iter()
+        .find_map(|(h, i)| if *h == header { Some(*i) } else { None })
 }
 
 pub fn current_time_string() -> String {
@@ -38,8 +44,67 @@ pub fn current_time_string() -> String {
 }
 
 impl TodoRecord {
+    const DEFAULT_PRIORITY: u8 = 3;
+
+    pub fn parse_record_line(line: &str) -> Result<TodoRecord, anyhow::Error> {
+        let values: Vec<&str> = line.split(',').collect();
+
+        if values.len() < 9 {
+            bail!("Input line does not contain enough fields".to_string());
+        }
+
+        let id = values[0].parse::<u32>()?;
+        let title = values[1].to_string();
+        let description = if values[2].is_empty() {
+            None
+        } else {
+            Some(values[2].to_string())
+        };
+        let due_date = if values[3].is_empty() {
+            None
+        } else {
+            Some(values[3].to_string())
+        };
+        let priority = if values[4].is_empty() {
+            Self::DEFAULT_PRIORITY
+        } else {
+            values[4].parse::<u8>()?
+        };
+        let status = values[5].parse::<bool>()?;
+        let created_at = if values[6].is_empty() {
+            current_time_string()
+        } else {
+            values[6].to_string()
+        };
+        let updated_at = if values[7].is_empty() {
+            current_time_string()
+        } else {
+            values[7].to_string()
+        };
+        let completed_at = if values[8].is_empty() {
+            None
+        } else {
+            Some(values[8].to_string())
+        };
+
+        Ok(TodoRecord {
+            id,
+            title,
+            description,
+            due_date,
+            priority,
+            status,
+            created_at,
+            updated_at,
+            completed_at,
+        })
+    }
+
     pub fn get_csv_header() -> String {
-        let headers: Vec<String> = HEADER_MAP.iter().map(|(h, _)| format!("\"{}\"", *h)).collect();
+        let headers: Vec<String> = HEADER_MAP
+            .iter()
+            .map(|(h, _)| format!("\"{}\"", *h))
+            .collect();
         let header_str = headers.join(",");
         format!("{}\n", header_str)
     }
@@ -59,9 +124,33 @@ impl TodoRecord {
         )
     }
 
-    pub fn get_newest_id(dbconf:&DBConfig){
-        let mut reader = fs::OpenOptions::read(true)
-        .open(&dbconf.get_db_path().t);
+    pub fn get_newest_id(dbconf: &DBConfig) -> Result<u32, anyhow::Error> {
+        let db_path = dbconf.get_db_path().context("Failed to find db file")?;
+        let db = fs::File::open(&db_path).context("Failed to open db file")?;
+        let reader = BufReader::new(&db);
+
+        let mut records: Vec<TodoRecord> = Vec::new();
+        let mut parse_errors = Vec::new();
+        for line in reader.lines() {
+            let line = line.context("Failed to read line from db file")?;
+            if !line.is_empty() {
+                let record = Self::parse_record_line(&line).map_err(|e| {
+                    parse_errors.push(format!("Error parsing record: {}", e));
+                    e
+                })?;
+                records.push(record);
+            }
+        }
+        if !parse_errors.is_empty() {
+            bail!(parse_errors.join("\n"));
+        }
+
+        let next_id = match records.iter().map(|r| r.id).max() {
+            Some(max_id) => max_id + 1,
+            None => 1, // 如果没有记录，从 1 开始
+        };
+
+        Ok(next_id)
     }
 }
 
